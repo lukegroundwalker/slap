ebc48e40d6d56a612779ae51bcfef39b442526c2
luke <1qazxcvbhu8765432@gmail.com>
2017-11-17 19:11:38 +0530

    Final

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Set the date we're counting down to
-	var countDownDate = new Date("Nov 25, 2017 17:00:00").getTime();
-	// var countDownDate = new Date("Nov 17, 2017 18:53:30").getTime();
+	// var countDownDate = new Date("Nov 25, 2017 17:00:00").getTime();
+	var countDownDate = new Date("Nov 17, 2017 19:07:00").getTime();
 	// Update the count down every 1 second
 	var x = setInterval(function() {
 
 	    // Get todays date and time
 	    var now = new Date().getTime();
-	    
+	    var tick = document.getElementById('tick');
 	    // Find the distance between now an the count down date
 	    var distance = countDownDate - now;
 	    
@@ -26,7 +26,7 @@
 	    	var intro = document.getElementsByClassName('intro');
 	        for(var i = 0; i < intro.length; i++)
 	            intro[i].innerHTML = '_________________';
-	        responsiveVoice.speak("That's all folks","UK English Male",{pitch: 0.5});
+	        document.getElementById('end').play();
 	        clearInterval(x);
 		    document.getElementById("days").innerHTML = "O";
 		    document.getElementById("hours").innerHTML = "U";
@@ -44,9 +44,7 @@
 	        responsiveVoice.speak(""+seconds+"", "UK English Male",{pitch: 0.1});
 	    }
 	    else{
-	    	// if(seconds%2==0)
-	        	// responsiveVoice.speak("tik");
-	     //    else
-	        	// responsiveVoice.speak("tock");
+	    	tick.play();
+	    	//tick.pause();
 	    }
 	}, 1000);
